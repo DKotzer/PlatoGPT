e6a18a80307fd12220b4886b76e5bdfa8c3dee16
Dylan <34380246+DKotzer@users.noreply.github.com>
2024-10-26 18:58:04 -0400

    cron time

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-    res.status(200).end('Hello Cron!');
+
+
+export default async function handler(req, res) {
+    await fetch("/api/testQA", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question: 'test', answer: 'test' }), // Use the local variable
+    })
+
+    res.status(200).end('Cron completed');
 }
